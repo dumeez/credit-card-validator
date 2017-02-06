98e5103f57c47ae80b9530b272cf26bacbf06f31
oumarPoulo <aob.diallo@gmail.com>
2017-02-06 12:28:53 +0100

    postinstall script

---
+++ post_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir dist
+node_modules/.bin/browserify src/index.js --standalone credit-card-validator -o dist/credit-card-validator.js
+node_modules/.bin/uglifyjs dist/credit-card-validator.js -o dist/credit-card-validator.min.js
