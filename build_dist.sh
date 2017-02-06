6584e0ef6edf6599b54ee11592c09377535a2b14
oumarPoulo <aob.diallo@gmail.com>
2017-02-06 12:40:59 +0100

    dist files

--- build_dist.sh
+++ build_dist.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-pwd
+rm -rf dist
 mkdir dist
-pwd
+npm install
 node_modules/.bin/browserify src/index.js --standalone credit-card-validator -o dist/credit-card-validator.js
-pwd
 node_modules/.bin/uglifyjs dist/credit-card-validator.js -o dist/credit-card-validator.min.js
